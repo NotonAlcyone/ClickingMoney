9780ac92f582425fe2c5469e07ab4ddd38ed1eac
Lee Dongyun <notonalcyone@gmail.com>
2017-08-01 19:03:38 +0900

    button with attribute&reload

--- main.py
+++ main.py
@@ -14,21 +14,31 @@ def reload():
 
 
 def buttonCall(parameter):
-	print(str(parameter + 1) + " 번째 버튼이 눌림")
+	level[parameter] += 1
+	print(str(parameter + 1) + " 번째 버튼이 눌려" + str(level[parameter] ) + " 레벨이 되었습니다.")
 
+	canvas.delete(button[parameter])
+	upgradeButton = Button(root,command = lambda i = parameter: buttonCall(parameter),text = str(level[parameter]))
+	button[parameter] = canvas.create_window(parameter*100+100,600, window = upgradeButton)
+
+
+level = []
 button = []
 def makeButton(num):
 	 
 	for i in range(0,num):
-		upgradeButton = Button(root,command = lambda i = i: buttonCall(i),text = "A")
+		upgradeButton = Button(root,command = lambda i = i: buttonCall(i),text = str(level[i]))
 		button.append(canvas.create_window(i*100+100,600,window = upgradeButton))
 
 
-
+def setButton(num):
+	for i in range(0,num):
+		level.append(1)
+	makeButton(num)
 
 B = Button(root,command = reload,text = "A")
 B_window = canvas.create_window(300,300,window = B)
 canvas.pack()
-makeButton(10)
+setButton(10)
 root.mainloop()
 
