c9f59671b30903d82dd54151cf4f17cd09bf869e
Lee Dongyun <notonalcyone@gmail.com>
2017-08-02 22:56:40 +0900

    DAILY COMMIT
    add crystal , gold system.
    and change button location by width of canvas automatically

--- main.py
+++ main.py
@@ -1,44 +1,118 @@
 from tkinter import *
 import os
 
+
+canvasSizeX = 1280
+canvasSizeY = 720
+totalButtonNum = 10
+buttonSpace = 95
 root = Tk()
-
-
-canvas =  Canvas(root, width = 1280, height = 720)
-
+canvas =  Canvas(root, width = canvasSizeX, height = 720)
 
 def reload():
-
 	for i in range(0,len(button)):
 		canvas.delete(button[i])
 
+def buyCall(parameter):
+	global crystal
+	payAmount = 100
+	if crystal > payAmount:
+		buttonCrystal[parameter] = True
+		print("구매가 완료되었습니다!")
+		crystal -= payAmount
+		crystalRefresh()
+		canvas.delete(button[parameter])
+		buttonPrint(parameter)
+	else:
+		print("크리스탈이 없어요!")
 
 def buttonCall(parameter):
-	level[parameter] += 1
-	print(str(parameter + 1) + " 번째 버튼이 눌려" + str(level[parameter] ) + " 레벨이 되었습니다.")
+	global gold
 
-	canvas.delete(button[parameter])
-	upgradeButton = Button(root,command = lambda i = parameter: buttonCall(parameter),text = str(level[parameter]))
-	button[parameter] = canvas.create_window(parameter*100+100,600, window = upgradeButton)
-
+	payAmount = level[parameter] * 10 # 지불가격
+	if gold >= payAmount:
+		level[parameter] += 1
+		print(str(parameter + 1) + " 번째 버튼이 눌려" + str(level[parameter] ) + " 레벨이 되었습니다.")
+		gold -= payAmount
+		goldRefresh()
+		canvas.delete(button[parameter])
+		buttonPrint(parameter)
+	else:
+		print("돈이 부족해요")
 
 level = []
 button = []
+buttonCrystal = []
 def makeButton(num):
-	 
-	for i in range(0,num):
-		upgradeButton = Button(root,command = lambda i = i: buttonCall(i),text = str(level[i]))
-		button.append(canvas.create_window(i*100+100,600,window = upgradeButton))
+	 for i in range(0,num):
+	 	buttonPrint(i)
+
+def buttonPrint(num):
+	buttonPoint = canvasSizeX / (totalButtonNum+1)
+	if buttonCrystal[num] == True :
+		upgradeButton = Button(root,compound = CENTER, command = lambda i = num: buttonCall(num),text = str(level[num]) + "레벨")
+
+	else:
+		upgradeButton = Button(root,compound = CENTER, command = lambda i = num: buyCall(num),text = "Buy")
+	button.append(canvas.create_window(num * buttonPoint + buttonPoint, 600,window = upgradeButton))
 
 
 def setButton(num):
 	for i in range(0,num):
-		level.append(1)
+		level.append(1) #시작레벨은 1
+		if i > 4:
+			buttonCrystal.append(False)
+		else :
+			buttonCrystal.append(True)
 	makeButton(num)
 
-B = Button(root,command = reload,text = "A")
-B_window = canvas.create_window(300,300,window = B)
+gold = 0
+goldText = None
+def goldSystem():
+	global gold
+	tmp = goldAmount()
+	gold += tmp
+	goldRefresh()
+	canvas.after(1000,goldSystem)
+	print("현 골드는 " + str(gold) + " 입니다")
+
+def goldAmount():
+	goldUp = 10
+	for i in range(0,len(level)):
+		goldUp += level[i]
+	return goldUp
+
+def goldRefresh():
+	global goldText
+	global gold
+	if goldText != None:
+		canvas.delete(goldText)
+	goldText = canvas.create_text(100, 100, text = gold)
+
+crystal = 0
+crystalText = None
+def crystalSystem(stat):
+	global crystal
+	if stat == "PTW":
+		crystal = 1000 #과금러
+	elif stat == "FTW":
+		crystal = 0 #무과금
+	crystalRefresh()
+
+def crystalRefresh():
+	global crystal
+	global crystalText
+	if crystalText != None:
+		canvas.delete(crystalText)
+	crystalText = canvas.create_text(200,100, text = "크리스탈: "+ str(crystal))
+
+
+
 canvas.pack()
-setButton(10)
+setButton(totalButtonNum)
+goldSystem()
+crystalSystem("FTW")
 root.mainloop()
+
+## makebutton 과 buttonCall 부분의 버튼 생성 부분 합치기
 
